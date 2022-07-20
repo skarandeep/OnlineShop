84b4ae8d19d0015d00bb2f9bd42504520d7a2e04
skarandeep <108695031+skarandeep@users.noreply.github.com>
2022-07-20 12:58:47 -0400

    L04-View Holder

--- app/src/main/java/com/androidtest/onlineshop/Controller/MainActivity.kt
+++ app/src/main/java/com/androidtest/onlineshop/Controller/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.Toast
 import com.androidtest.onlineshop.Adapters.CategoryAdapter
 import com.androidtest.onlineshop.Model.Category
 import com.androidtest.onlineshop.R
@@ -20,5 +21,16 @@ class MainActivity : AppCompatActivity() {
         val categoryListView = findViewById<ListView>(R.id.categoryListView)
         categoryListView.adapter = adapter
 
+        /*
+        categoryListView.setOnItemClickListener { parent, view, position, id ->
+            val category = DataService.categories[position]
+            Toast.makeText(this, "You clicked on ${category.title} cell", Toast.LENGTH_SHORT).show()
+        }
+        */
+
+
     }
+
 }
+
+abstract class LayoutManager()
